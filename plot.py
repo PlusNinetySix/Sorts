229a5dc885f2ba33a87e15f8868943743c6d7ac5
JGPNS <PlusNinetySix@users.noreply.github.com>
2017-11-20 11:52:58 -0600

    Added comments.

--- plot.py
+++ plot.py
@@ -3,43 +3,51 @@ import matplotlib.pyplot as plt
 from random import sample
 from time import perf_counter
 
+#Generates numbers up to 20000.
 randnum = sample(range(20000), k=20000)
 
+#This function starts the timer, and is measured in milliseconds.
 def timer(algorithm, randnum):
     start = perf_counter()
     algorithm(randnum)
     stop = perf_counter()
-    timer = stop - start
+    timer = ((stop - start) * 1000)
     return timer
 
+#All of these put the numbers from the algorithms in an array.
 y=[]
-x=[]
-z=[]
-w=[]
-u=[]
-o=[]
+quick=[]
+selection=[]
+merge=[]
+bubble=[]
+insertion=[]
 
-for n in range (1000,20000,1000): 
+#These for loops put the arrays into a plottable form.
+for n in range (1000,20001,1000): 
     y.append(n)
-for n in range (1000,20000,1000):
-    x.append(timer(sort.quicksort, randnum[0:n]))
-for n in range (1000,20000,1000):
-    z.append(timer(sort.selection_sort, randnum[0:n]))
-for n in range (1000,20000,1000):
-    w.append(timer(sort.merge_sort, randnum[0:n]))
-for n in range (1000,20000,1000):
-    u.append(timer(sort.bubble_sort, randnum[0:n]))
-for n in range (1000,20000,1000):
-    o.append(timer(sort.insertion_sort, randnum[0:n]))
+for n in range (1000,20001,1000):
+    quick.append(timer(sort.quicksort, randnum[0:n]))
+for n in range (1000,20001,1000):
+    selection.append(timer(sort.selection_sort, randnum[0:n]))
+for n in range (1000,20001,1000):
+    merge.append(timer(sort.merge_sort, randnum[0:n]))
+for n in range (1000,20001,1000):
+    bubble.append(timer(sort.bubble_sort, randnum[0:n]))
+for n in range (1000,20001,1000):
+    insertion.append(timer(sort.insertion_sort, randnum[0:n]))
 
-plt.plot(x, y, marker="^", color="purple", label="quick sort")
-plt.plot(z, y, marker="*", color="blue", label="selection sort")
-plt.plot(w, y, marker="p", color="green", label="merge sort")
-plt.plot(u, y, marker="D", color="yellow", label="bubble sort")
-plt.plot(o, y, marker="s", color="red", label="insertion sort")
+#This plots all of the sorting algortihms
+plt.plot(quick, y, marker="^", color="purple", label="quick sort")
+plt.plot(selection, y, marker="*", color="blue", label="selection sort")
+plt.plot(merge, y, marker="p", color="green", label="merge sort")
+plt.plot(bubble, y, marker="D", color="yellow", label="bubble sort")
+plt.plot(insertion, y, marker="s", color="red", label="insertion sort")
 
+#This sets the labels and turns on the grid, then shows the graph.
 plt.xlabel("Time (ms)")
 plt.ylabel("Number of Elements")
 plt.title("Time of Various Sorting Algorithms")
+plt.grid(b='on', which='major', axis='both')
+plt.minorticks_on()
 plt.legend()
 plt.show()
